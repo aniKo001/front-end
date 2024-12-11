a01fe27cee31c6a79e037dad080ba365ed2dfc1c
new name <nomioyumshigen0117@gmail.com>
2024-12-11 13:25:28 +0800

    bie daalt

---
+++ anar/pages/biydaltana.js
@@ -0,0 +1,43 @@
+import { useRouter } from "next/router";
+
+const data = [
+  {
+    id: 1,
+    firstname: "",
+    lastname: "",
+    age: 16,
+    hobby: "",
+  },
+];
+
+export default function Home() {
+  const router = useRouter();
+  return (
+    <div>
+      <p className="text-center font-bold text-xl my-10">Бие даалт</p>
+      <div className="flex items-center justify-center">
+        <button
+          className="border rounded-lg bg-gray-200 p-4 font-bold mb-2 hover:bg-blue-200"
+          onClick={() => {
+            router.push("squash");
+          }}
+        >
+          Week ruu shiljine
+        </button>
+      </div>
+      <div className="flex items-center flex-col space-y-4 border rounde-lg mx-20">
+        <p className="font-semibold my-4">Шаардлага:</p>
+        <li>UI/UX design (10 оноо)</li>
+        <li>
+          Ангийнхаа бүх хүүхдийн мэдээллийг JSON датагаар оруулах (5 оноо)
+        </li>
+        <li>
+          Ангийнхаа бүх хүүхдийн мэдээллийг student хуудсанд List болон Grid
+          жагсаалтаар харуулах (5 оноо)
+        </li>
+        <li>Сурагчдийн нэрээр хайдаг байх (10 оноо)</li>
+        <li>Github дээр байршсан байх (5оноо)</li>
+      </div>
+    </div>
+  );
+}
--- anar/pages/tmep.js
+++ anar/pages/tmep.js
@@ -9,9 +9,9 @@ export default function Home() {
                    <h1 className="text-xl text-whtie">Service</h1>
                    <h1 className="text-xl text-whtie">Portfolio</h1>
                    <h1 className="text-xl text-whtie">Team</h1>
-                   <button className="flex text-xl text-whtie">
+                   <button className="flex text-xl text-whtie justify-center items-center">
                     Drop down 
-                    <img src="https://cdn-icons-png.flaticon.com/512/54/54785.png" className="w-6 h-6 "></img></button>
+                    <img src="https://cdn-icons-png.flaticon.com/512/54/54785.png" className="w-4 h-4 flex justify-center items-center "></img></button>
                    <h1 className="text-xl text-whtie">Contact</h1>
                    <button className="border-2 border-blue-300 rounded-xl border-inherit text-xl text-white">Get Started</button>
                 </div>
@@ -22,8 +22,8 @@ export default function Home() {
                         Your Business</p>
                     <p className="text-slate-500 text-3xl">We are team of talented designers making
                          websites with Bootstrap</p>
-                    <button className="text-white border-2 border-inherit rounded-full mx-8 border-blue-400 bg-blue-400 text-2xl">Get Started</button>
-                    <button className="flex text-white text-2xl mx-8">
+                    <button className="flex text-white border-2 border-inherit rounded-full m-4 border-blue-400 bg-blue-400 text-2xl">Get Started</button>
+                    <button className="flex text-white text-2xl m-4">
                         <img src="https://cdn-icons-png.flaticon.com/512/0/375.png" className="w-6 h-6"></img>
                         Watch Video</button>
                 </div>
@@ -32,6 +32,19 @@ export default function Home() {
                          className="w-80 h-80"
                     ></img>
                 </div>
+            </div>
+            <div className="flex w-full h-24 bg-gray-300 space-x-16 justify-center items-center">
+                <p className="text-black text-5xl m-8">myob</p>
+                <p className="text-black text-5xl m-8">BELIMO</p>
+                <p className="text-black text-5xl m-8">LifeGroup</p>
+                <p className="text-black text-5xl m-8">Lilly</p>
+                <p className="text-black text-5xl m-8">Citrus</p>
+                <p className="text-black text-5xl m-8">Trustly</p>
+            </div>
+            <div className="w-full h-96 bg-white">
+                <div></div>
+                <div></div>
+                <div></div>
             </div>
         </div>
           

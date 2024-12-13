8849e7981dcf6f498c2944760b27b18345b01150
new name <nomioyumshigen0117@gmail.com>
2024-12-13 14:13:17 +0800

    ners bichsen

---
+++ anar/pages/biydaalt.js
@@ -0,0 +1,159 @@
+import { useRouter } from "next/router";
+
+const data = [
+  {
+    id: 1,
+    firstname: "",
+    lastname: "",
+    age: 16,
+    hobby: "",
+  },
+];
+
+export default function Home() {
+  const router = useRouter();
+  return (
+    const data = [
+        {
+            id: 1,
+            firstname: "Анар",
+            lastname: "Ганбаатар",
+            age: 16,
+            hobby: "сагсан бөмбөг"
+        },
+        {
+          id: 2,
+          firstname: "Амар-түвшин",
+          lastname: "Сэргэлэн",
+          age: 16,
+          hobby: "Код бичих , Вал тоглох"
+        },
+        {
+          id: 3,
+          firstname: "Анар-эрдэнэ",
+          lastname: "Гантулга",
+          age: 16,
+          hobby: "Код бичих , Вал тоглох"
+        },
+        {
+          id: 4,
+          firstname: "Батмэнд",
+          lastname: "Батбаатар",
+          age: 16,
+          hobby: "сагсан бөмбөг"
+        },
+        {
+          id: 5,
+          firstname: "Далай-сүрэн",
+          lastname: "Эрдэнэтулга",
+          age: 16,
+          hobby: "Код бичих , Воллеволл тоглох"
+        },
+        {
+          id: 6,
+          firstname: "Тэмүүжин",
+          lastname: "Батчулуун",
+          age: 16,
+          hobby: "Код бичих , Вал тоглох"
+        },
+        {
+          id: 7,
+          firstname: "Тэмүүжин",
+          lastname: "Шинэбаяр",
+          age: 16,
+          hobby: "Код бичих , ширээний тэннис"
+        },
+        {
+          id: 8,
+          firstname: "Тэмүүжин",
+          lastname: "Энхдөл",
+          age: 16,
+          hobby: "сагсан бөмбөг"
+        },
+        {
+          id: 9,
+          firstname: "Тэмүүлэн",
+          lastname: "Саруул-од",
+          age: 16,
+          hobby: "сагсан бөмбөг"
+        },
+        {
+          id: 10,
+          firstname: "Очир-эрдэнэ",
+          lastname: "Ганбаатар",
+          age: 16,
+          hobby: "сагсан бөмбөг"
+        },
+        {
+          id: 11,
+          firstname: "Ханбилэг",
+          lastname: "Дамдинсүрэн",
+          age: 16,
+          hobby: "Код бичих , Вал тоглох"
+        },
+        {
+          id: 12,
+          firstname: "Шинэ-эрдэнэ",
+          lastname: "Шинэлундаа",
+          age: 16,
+          hobby: "сагсан бөмбөг"
+        },
+        {
+          id: 13,
+          firstname: "Цэлмэг",
+          lastname: "Ганзориг",
+          age: 16,
+          hobby: "сагсан бөмбөг"
+        },
+        {
+          id: 14,
+          firstname: "Энх-учрал",
+          lastname: "Амгаланбаатар",
+          age: 16,
+          hobby: "Код бичих , Вал тоглох"
+        },
+        {
+          id: 15,
+          firstname: "Энх-төгс",
+          lastname: "Урантулга",
+          age: 16,
+          hobby: "сагсан бөмбөг"
+        },
+        {
+          id: 16,
+          firstname: "Энхжав",
+          lastname: "Батжаргал",
+          age: 16,
+          hobby: "Код бичих , Вал тоглох"
+        },
+        {
+          id: 17,
+          firstname: "Эмили",
+          lastname: "Энхтөр",
+          age: 16,
+          hobby: "Код бичих , Вал тоглох"
+        },
+        {
+          id: 18,
+          firstname: "Чой-одсэр",
+          lastname: "Алтанбагана",
+          age: 16,
+          hobby: "Код бичих , Вал тоглох"
+        },
+        {
+          id: 19,
+          firstname: "Тэргэл",
+          lastname: "Ганболд",
+          age: 16,
+          hobby: "Код бичих , Вал тоглох"
+        },
+        {
+          id: 20,
+          firstname: "Төгс-асралт",
+          lastname: "Эрдэнэ-туяа",
+          age: 16,
+          hobby: "Код бичих , Вал тоглох"
+        },
+    ],
+  );
+}
